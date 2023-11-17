1d6697c652633a274911c3e39dae43ebb388afc4
mael <mael@morin.fr>
2023-11-17 17:43:22 +0100

    V1.0

---
+++ kPPV.py
@@ -0,0 +1,51 @@
+""" programme à compléter du kPPV"""
+import csv
+import math
+
+nbExParClasse = 50
+nbApprent = 25
+nbCaract = 4
+nbClasse = 3
+
+def lectureFichierCSV():
+    with open("iris.data", 'r')as fic:
+        lines = csv.reader(fic)
+        dataset = list(lines)
+    print(dataset[0], len(dataset))
+    for i in range(len(dataset)):
+        for j in range(nbCaract):
+            dataset[i][j] = float(dataset[i][j])
+            #print (dataset[i][j])
+    print(dataset[0])
+    return(dataset)
+
+def calculdistances(data, dataset):
+    """ retourne les distances entre data et la partie apprentissage de dataset"""
+    distances = []
+    for i in range(nbClasse*nbExParClasse):
+        if i % nbExParClasse < nbApprent:
+            dist = 0
+            for k in range(nbCaract):
+                dist += (data[k]-dataset[i][k])**2
+            distances.append(dist**0.5)
+    return distances
+
+def calculClasse(distances):
+    """ retourne le numéro de la classe déterminé à partir des distances """
+    
+	#-------- A faire... --------
+
+    return(classe)
+
+if __name__ == "__main__":
+    print("Début programme kPPV")
+    dataset = lectureFichierCSV()
+    print (dataset[0][0])
+    distance = calculdistances([5.0,3.6,1.4,0.2],dataset)
+    print (distance)
+
+    # Calcule et affiche la matrice de confusion et le taux de reco
+    
+	#-------- A faire... --------
+
+#--------------------------------- Fin kPPV -----------------------------------
